a520e45151c998147098e3e4578b9faf3f7e0643
Aayush <aayush.jalan@tavisca.com>
2022-10-26 16:06:52 +0530

    Middleware/Configuration added to Startup.cs with 4 Test case passing

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -32,7 +32,7 @@ namespace WishList
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseRouting();
-            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
             //app.Run(async (context) =>
             //{
             //    await context.Response.WriteAsync("Hello World!");
